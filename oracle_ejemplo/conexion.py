a6c220ec2413d188ad92f0f2c51d0f513d8a8dfd
svpu-23 <vicente.sepulveda23@inacapmail.cl>
2025-11-06 11:18:54 -0300

    conexion sql

---
+++ oracle_ejemplo/conexion.py
@@ -0,0 +1,14 @@
+import oracledb
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+username = os.getenv("ORACLE_USER")
+dsn = os.getenv("ORACLE_DSN")
+password = os.getenv("ORACLE_PASSWORD")
+
+with oracledb.connect(user=username, password=password, dsn=host) as connection:
+    with connection.cursor() as cursor:
+        sql = "select sysdate from dual"
+        for r in cursor.execute(sql):
+            print(r)
